a4632355d3a18d8505c65e2e3f2304cd856806eb
CJ Lin <ejames.lin@gmail.com>
2021-07-04 02:19:37 +0800

    Refresh directory at active scenePhase

--- AppActivityViewer/AppActivityViewer/View/DirectoryView.swift
+++ AppActivityViewer/AppActivityViewer/View/DirectoryView.swift
@@ -11,42 +11,55 @@ struct DirectoryView: View {
 
     @State var fileList: [String]?
 
+    @Environment(\.scenePhase) var scenePhase
+
     var body: some View {
         NavigationView {
-            ZStack {
-                if let fileList = fileList, !fileList.isEmpty {
-                    List {
-                        Section(header: Text("Reports in directory")) {
-                            ForEach(fileList, id: \.self) { file in
-                                NavigationLink(
-                                    destination: AppsView(fileName: file),
-                                    label: { Text(file) }
-                                )
+            ZStack(alignment: .top) {
+                if let fileList = fileList {
+                    if fileList.isEmpty {
+                        Color.clear // force ZStack to fill parent
+                        EmptyView()
+                    } else {
+                        List {
+                            Section(header: Text("Reports in directory")) {
+                                ForEach(fileList, id: \.self) { file in
+                                    NavigationLink(
+                                        destination: AppsView(fileName: file),
+                                        label: { Text(file) }
+                                    )
+                                }
                             }
                         }
-                    }
-                } else {
-                    VStack(alignment: .leading) {
-                        Text("No Report in App\n").font(.title)
-
-                        Text("1. Turn on activity record\n").font(.title3)
-
-                        Text("Settings -> Privacy -> Record App Activity,")
-                        Text("and turn on Record App Activity\n")
-
-                        Text("2. Share to this App\n").font(.title3)
-                        Text("Click Save App Activity")
-
-                        Text("At the share sheet, choose ") + Text("AppActivity").bold()
                     }
                 }
             }
             .navigationTitle("App Activity Viewer")
-            .task {
-                async {
-                    try fileList = await ViewModel.jsonFileListInApp()
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .active {
+                    async {
+                        try fileList = await ViewModel.jsonFileListInApp()
+                    }
                 }
             }
+        }
+    }
+}
+
+struct EmptyView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("No Report in App\n").font(.title)
+
+            Text("1. Turn on activity record\n").font(.title3)
+
+            Text("Settings -> Privacy -> Record App Activity,")
+            Text("and turn on Record App Activity\n")
+
+            Text("2. Share to this App\n").font(.title3)
+            Text("Click Save App Activity")
+
+            Text("At the share sheet, choose ") + Text("AppActivity").bold()
         }
     }
 }
